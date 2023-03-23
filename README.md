e06f371770dbc9b4d009c3ac2ac77bad02a06094
choi8327 <choi8327@gmail.com>
2023-03-23 16:26:00 +0900

    회원가입 및 로그인 추가

---
+++ README.md
@@ -0,0 +1,24 @@
+# flutter_testshop
+
+A new Flutter project.
+
+## Getting Started
+
+This project is a starting point for a Flutter application.
+
+A few resources to get you started if this is your first Flutter project:
+
+- [Lab: Write your first Flutter app](https://docs.flutter.dev/get-started/codelab)
+- [Cookbook: Useful Flutter samples](https://docs.flutter.dev/cookbook)
+
+For help getting started with Flutter development, view the
+[online documentation](https://docs.flutter.dev/), which offers tutorials,
+samples, guidance on mobile development, and a full API reference.
+
+# 플러터로 연습으로 만드는 쇼핑몰
+
+## 구현 기능
+
+- 회원가입 / 로그인 
+- 상품 조회 / 상품 조회
+- 장바구니
---
+++ lib/main.dart
@@ -0,0 +1,37 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:provider/provider.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_testshop/screens/register.dart';
+import 'package:flutter_testshop/firebase_options.dart';
+import 'package:flutter_testshop/models/auth.dart';
+import 'screens/index.dart';
+import 'screens/login.dart';
+import 'screens/splash.dart';
+import 'screens/register.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => FirebaseAuthProvider()),
+      ],
+      child: MaterialApp(
+        title: 'Flutter Shop',
+        routes: {
+          '/index': (context) => IndexScreen(),
+          '/login': (context) => LoginScreen(),
+          '/splash': (context) => SplashScreen(),
+          '/register': (context) => RegisterScreen(),
+        },
+        initialRoute: '/splash',
+      ),
+    );
+  }
+}
